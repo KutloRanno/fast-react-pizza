e827ba21f30522de5907205205bc99d20b1e0b04
KutloRanno <abc20-010@thuto.bac.ac.bw>
2024-07-23 15:23:59 +0200

    The first and probably final commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+import OrderItem from "./features/order/OrderItem";
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error.jsx";
+import { loader as orderLoader } from "./features/order/Order.jsx";
+import { action as updateOrderAction } from "./features/order/UpdateOrder.jsx";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router}></RouterProvider>;
+}
+
+export default App;
